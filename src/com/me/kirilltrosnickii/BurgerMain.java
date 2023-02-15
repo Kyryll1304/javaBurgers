12e7722babed9fa666f4d80e9345910ec4310d44
Kyryll1304 <kirilltrosnickii@me.com>
2023-02-16 00:18:42 +0200

    Classes refractored

--- src/com/me/kirilltrosnickii/BurgerMain.java
+++ src/com/me/kirilltrosnickii/BurgerMain.java
@@ -2,9 +2,9 @@ package com.me.kirilltrosnickii;
 
 public class BurgerMain {
     public static void main(String[] args) {
-        Burger burger = new Burger("булка", "м'ясо", "сир", "зелень", "майонез");
+        Burger burger = new Burger("булка", "м'ясо", "сир", "зелень", "майонез", true);
         Burger burger1 = new Burger("булка", "м'ясо", "сир", "зелень");
-        Burger burger2 = new Burger("булка", "м'ясо", "сир", "зелень", "майонез", 2);
+        Burger burger2 = new Burger("булка", "м'ясо", "сир", "зелень", "майонез", false);
     }
 }
 
@@ -14,38 +14,31 @@ class Burger {
     private String cheese;
     private String green;
     private String mayo;
-    private int id;
 
-    public Burger(String bun, String meat, String cheese, String green, String mayo) {
-        this.id = 0;
+
+    public Burger(String bun, String meat, String cheese, String green, String mayo, boolean doubleMeat) {
+
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.green = green;
         this.mayo = mayo;
-        System.out.println("Склад звичайного бургера є " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo);
+        if (doubleMeat) {
+            System.out.println("Склад подвійного бургера є " + bun + ",2 " + meat + ", " + cheese + ", " + green + ", " + mayo);
+        } else {
+            System.out.println("Склад звичайного бургера є " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo);
+        }
+
     }
 
     public Burger(String bun, String meat, String cheese, String green) {
-        this.id = 1;
+
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.green = green;
         this.mayo = "";
         System.out.println("Склад диєтичного бургера є " + bun + ", " + meat + ", " + cheese + ", " + green);
-    }
-
-    public Burger(String bun, String meat, String cheese, String green, String mayo, int meatAmount) {
-        this.id = 2;
-        this.bun = bun;
-        this.meat = meat;
-        this.cheese = cheese;
-        this.green = green;
-        this.mayo = mayo;
-        if (meatAmount == 2) {
-            System.out.println("Склад подвійного бургера є " + bun + ", " + meat + ", " + meat + ", " + cheese + ", " + green + ", " + mayo);
-        }
     }
 
 
